dea664cbc27994f6d40e450b3376788638f47e79
Erik de Vries <erikdv@online.nl>
2024-07-18 20:21:46 +0200

    New message layout

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,39 @@
+import {Component, Input} from '@angular/core';
+import {Message} from "./models/message";
+import {formatDate, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgIf
+  ],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css'
+})
+export class MessageComponent {
+  @Input() message: Message | undefined;
+
+  constructor() {
+  }
+
+  showContent = true;
+
+  showMessageContent(): void {
+    this.showContent = !this.showContent;
+  }
+
+  options: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+  };
+
+  formatDate(date: Date): string {
+    date = new Date(date);
+    return date.toLocaleString("nl-NL", this.options).replace(",", "");
+  }
+}
