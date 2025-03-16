a144b257b0efe7194d5c89b80a042833084df57f
Erik de Vries <erikdv@online.nl>
2025-03-16 15:08:02 +0100

    Categories - work in progress

---
+++ src/app/messages-categories/all-messages-button/all-messages-button.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {CategoryService} from "../../service/category.service";
+
+@Component({
+  selector: 'app-all-messages-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-messages-button.component.html',
+  styleUrl: './all-messages-button.component.css'
+})
+export class AllMessagesButtonComponent {
+
+  selectedCategory: string = "all"
+  @Output() categorySelectedEvent = new EventEmitter<string>();
+
+  constructor(private dataService:CategoryService) {}
+
+
+
+  selectCategory(category: string) {
+    this.selectedCategory = category
+    this.categorySelectedEvent.emit(this.selectedCategory);
+  }
+}
--- src/app/messages-categories/messages-categories-container.component.ts
+++ src/app/messages-categories/messages-categories-container.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MessagesComponent} from "./messages/messages.component";
 import {CategoriesComponent} from "./categories/categories.component";
+import {AllMessagesButtonComponent} from "./all-messages-button/all-messages-button.component";
 
 @Component({
   selector: 'app-messages-categories',
   standalone: true,
   imports: [
     MessagesComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    AllMessagesButtonComponent
   ],
   templateUrl: './messages-categories-container.component.html',
   styleUrl: './messages-categories-container.component.css'
