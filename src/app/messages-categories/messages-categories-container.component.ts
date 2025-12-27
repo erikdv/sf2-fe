1955f260ecc59e31fc8d0dabeaf6bbb2c44811d4
Erik de Vries <erikdv@online.nl>
2025-12-27 21:19:14 +0100

    Fixed active category button after sending a new message

--- src/app/messages-categories/messages-categories-container.component.ts
+++ src/app/messages-categories/messages-categories-container.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MessagesComponent} from "./messages/messages.component";
 import {CategoriesComponent} from "./categories/categories.component";
 import {AllMessagesButtonComponent} from "./all-messages-button/all-messages-button.component";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-messages-categories',
@@ -13,8 +14,17 @@ import {AllMessagesButtonComponent} from "./all-messages-button/all-messages-but
     templateUrl: './messages-categories-container.component.html',
     styleUrl: './messages-categories-container.component.css'
 })
-export class MessagesCategoriesContainerComponent {
+export class MessagesCategoriesContainerComponent implements OnInit {
   activeCategory = 'all'
+
+  constructor(private cookieService: CookieService) {}
+
+  ngOnInit() {
+    const storedCategory = this.cookieService.get('category');
+    if (storedCategory) {
+      this.activeCategory = storedCategory;
+    }
+  }
 
   receiveCategoryEvent(event: string) {
     this.activeCategory = event
